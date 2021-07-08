24d21834bce2697823b7c5d8c3b5c17eea201830
Nikita Mounier <nikita.mounier@gmail.com>
2021-07-08 19:09:52 +0200

    Add OrderedCollections dependency to target.

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.4.0"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.1.3"),
+    .package(url: "https://github.com/apple/swift-collections", from: "0.0.3"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.1.0"),
   ],
   targets: [
@@ -27,6 +28,7 @@ let package = Package(
       dependencies: [
         "CasePaths",
         "CombineSchedulers",
+        "OrderedCollections",
         "XCTestDynamicOverlay",
       ]
     ),
